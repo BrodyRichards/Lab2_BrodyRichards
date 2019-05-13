bad980c237708e91fb0b824b71e0946e40a23ae8
Brody <brparich@ucsc.edu>
2019-05-12 19:24:09 -0700

    1st post

---
+++ Lab2/Assets/Scripts/toggler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggler : MonoBehaviour
+{
+    bool toggle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toggle = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        this.GetComponent<MeshRenderer>().enabled = toggle;
+
+
+        if (Input.GetKeyDown("space"))
+        {
+            toggle = !toggle; 
+        }
+    }
+}
